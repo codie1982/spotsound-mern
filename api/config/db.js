a30a155c5904609fd5b46852b3f37d1d69563c23
codie1982 <engin_erol@hotmail.com>
2024-08-31 09:19:07 +0300

    adding mongoose strictQuery value

--- api/config/db.js
+++ api/config/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
-
+mongoose.set('strictQuery', true);
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI,{dbName:process.env.DB_SPOTSOUND})
